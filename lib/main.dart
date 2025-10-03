051e2ee1eeaeb03b36da19471b9ae1ecc21c9840
Guerrero Vázquez <lguerrero@CCLLABEL.CCLIND.NET>
2025-10-03 17:09:19 -0600

    Cambios en pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,140 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(children: [
-            Text('Hello World!'),
-           
-          ] 
+  Widget buildOccupancyCard(String title, double percent, int people, Color color) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const Icon(Icons.search, size: 20, color: Colors.black54),
+              ],
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 160,
+              width: 160,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  PieChart(
+                    PieChartData(
+                      startDegreeOffset: -90,
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 65,
+                      sections: [
+                        PieChartSectionData(
+                          value: percent,
+                          color: color,
+                          radius: 20,
+                          showTitle: false,
+                        ),
+                        PieChartSectionData(
+                          value: 100 - percent,
+                          color: Colors.grey.shade300,
+                          radius: 20,
+                          showTitle: false,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${percent.toInt()} %',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '$people people',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "2025-07-16 15:05:24",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey.shade600,
+              ),
+            ),
+          ],
         ),
       ),
-      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        toolbarHeight: 80,
+        title: Row(
+  mainAxisAlignment: MainAxisAlignment.end, // todo alineado a la derecha
+  children: [
+    Text(
+      'INDITEX',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    ),
+    const SizedBox(width: 400),
+    Image.asset(
+      'assets/images/storeops_logo2.png',
+      height: 40,
+    ),
+    const SizedBox(width: 400),
+    Image.asset(
+      'assets/images/checkpoint_logo.png',
+      height: 40,
+    ),
+  ],
+)
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildOccupancyCard("Gimnasio", 75, 310, Colors.green),
+            buildOccupancyCard("Auditorio", 43, 234, Colors.green),
+          ],
+        ),
+      ),
     );
   }
 }
